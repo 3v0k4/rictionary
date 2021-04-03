f1b3c646f35216d89b4865e55963075cccae6a34
3v0k4 <riccardo.odone@gmail.com>
2021-04-03 13:05:43 +0200

    remove: language dropdown

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,16 +1,11 @@
 class LandingController < ApplicationController
   def index
     @view_model = Fetch.new.call(query)
-    @language = language
   end
 
   private
 
   def query
     @query ||= params.fetch(:query, "")
-  end
-
-  def language
-    @language ||= params.fetch(:language, "")
   end
 end
