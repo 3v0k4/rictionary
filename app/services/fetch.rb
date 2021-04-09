95f2736f11d1e9c479339d163e7dbc8d497fa595
3v0k4 <riccardo.odone@gmail.com>
2021-04-09 22:43:31 +0200

    refactor: serial strategy pattern

--- app/services/fetch.rb
+++ app/services/fetch.rb
@@ -2,28 +2,34 @@ class Fetch
   def call(query)
     return NoQueryViewModel.new if query.empty?
 
-    q = query
-    html = FetchWiktionaryPage.new.call(q)
-    return wiktionary_view_model(q, html) unless html.nil?
-
-    q = query.downcase
-    html = FetchWiktionaryPage.new.call(q)
-    return wiktionary_view_model(q, html) unless html.nil?
-
-    q = CorrectQueryViaWiktionary.new.call(query.downcase)
-    if q
-      html = FetchWiktionaryPage.new.call(q)
-      return wiktionary_view_model(q, html) unless html.nil?
+    catch(:found) do
+      try_wiktionary(query)
+      try_wiktionary(query.downcase)
+      try_wiktionary_with_correction(query)
+      try_babla(query)
+      NotFoundViewModel.new(query: query)
     end
-
-    q = query
-    fetched = FetchBablaTranslations.new.call(q)
-    return BablaViewModel.new(query: fetched.corrected, translations: fetched.translations) unless fetched.nil?
-
-    NotFoundViewModel.new(query: query)
   end
 
   private
+
+  def try_wiktionary(query)
+    html = FetchWiktionaryPage.new.call(query)
+    return if html.nil?
+    throw :found, wiktionary_view_model(query, html)
+  end
+
+  def try_wiktionary_with_correction(query)
+    q = CorrectQueryViaWiktionary.new.call(query.downcase)
+    return if q.nil?
+    try_wiktionary(q)
+  end
+
+  def try_babla(query)
+    fetched = FetchBablaTranslations.new.call(query)
+    return if fetched.nil?
+    throw :found, BablaViewModel.new(query: fetched.corrected, translations: fetched.translations)
+  end
 
   def wiktionary_view_model(query, html)
     parsed = ParseWiktionaryHtml.new.call(html)
